b23b049b2b2271c0f31e14bc264d196411a4952f
Thomas Lehner <thomas.lehner@justremotephone.com>
2017-10-01 22:08:49 +0200

    Fix crash for Android O: Call upio_start_stop_timer only if timer was created.
    Change-Id: I16cef815364d784f306ff03cd16d6397406e6603

--- src/upio.c
+++ src/upio.c
@@ -506,7 +506,8 @@ void upio_set(uint8_t pio, uint8_t action, uint8_t polarity)
             else
             {
                 /* arm user space timer based on action */
-                upio_start_stop_timer(action);
+                if (lpm_proc_cb.timer_created)
+                	upio_start_stop_timer(action);
             }
 #endif
 
